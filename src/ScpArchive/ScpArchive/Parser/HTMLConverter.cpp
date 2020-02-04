6316300498d666ffe4d304d20ab613a0112dc14f
danielbatterystapler <danielbatterystapler@gmail.com>
2020-02-04 00:41:12 -0500

    fix bug with annotated source code colors

--- src/ScpArchive/ScpArchive/Parser/HTMLConverter.cpp
+++ src/ScpArchive/ScpArchive/Parser/HTMLConverter.cpp
@@ -33,7 +33,7 @@ namespace Parser{
 	}
 	
 	namespace{
-		std::array<std::string, 50> colors = { 
+		std::array<std::string, 48> colors = { 
 			"FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF",
 			"800000", "008000", "000080", "808000", "800080", "008080",
 			"C00000", "00C000", "0000C0", "C0C000", "C000C0", "00C0C0",
