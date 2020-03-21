211f2ea32713b2d2d74c70c8793c5718514625c0
danielbatterystapler <danielbatterystapler@gmail.com>
2020-03-21 02:09:26 -0400

    fix bug related to deleted authors

--- src/ScpArchive/ScpArchive/Parser/DatabaseUtil.cpp
+++ src/ScpArchive/ScpArchive/Parser/DatabaseUtil.cpp
@@ -16,8 +16,13 @@ namespace Parser{
 					out.type = ShownAuthor::Type::User;
 					break;
 			}
-			out.shownName = author.name;
-			out.linkName = normalizePageName(out.shownName);
+			if(author.name == ""){//if the author has a blank name, that means it should be treated as deleted
+				out.type = ShownAuthor::Type::Deleted;
+			}
+			else{
+				out.shownName = author.name;
+				out.linkName = normalizePageName(out.shownName);
+			}
 		}
 		else{
 			out.type = ShownAuthor::Type::Deleted;
